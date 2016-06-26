4c3721b5324fbc927cfab438124370da0478bc7b
Dmitry Birin <dmitry.birin@gmail.com>
2016-06-26 21:49:46 +0300

    working extension, no UI

---
+++ background.js
@@ -0,0 +1,17 @@
+'use strict'
+let redirectList = [
+    {ruleName: "FB->messenger", urlContains:"facebook.com", redirectToUrl:"https://messenger.com"},
+    {ruleName: "vk->edted", urlContains:"vk.com", redirectToUrl:"https://ed.ted.com"},
+    {ruleName: "meduza->edted", urlContains:"meduza.io", redirectToUrl:"https://ed.ted.com"},
+    ]
+chrome.webNavigation.onCommitted.addListener(function(details){
+    if (details.transitionQualifiers.includes('from_address_bar')
+        || details.transitionQualifiers.includes('forward_back'))
+    {
+        var currentRuleList = redirectList.filter((rule) => details.url.indexOf(rule.urlContains) !== -1)
+        if (currentRuleList.length===1)
+        {
+            chrome.tabs.update({"url": currentRuleList[0].redirectToUrl});
+        }
+    }
+}, {url:redirectList.map((rule)=>Object.create({urlContains: rule.urlContains}))});
